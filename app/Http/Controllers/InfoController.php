81d22856dc2e13b25de5ada48826b4b925082c32
Pablo Ledesma <pabloledes83@gmail.com>
2015-05-09 09:35:20 -0500

    Inicio del nuevo informe

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -263,5 +263,41 @@ class InfoController extends Controller {
         
         return view('info.sin_resultados', compact('input'));
     }
+    
+    /**
+    * Muestra el informe de certificado ica
+    ***/ 
+    public function certificado_ica()
+    {
+        $query = "SELECT 
+            dbo.MOVCONT3.DOCCOD, 
+            dbo.MOVCONT3.MvCNro,            
+            dbo.MOVCONT3.MvCFch, 
+            dbo.MOVCONT2.MvCNat, 
+            dbo.CUENTAS.CntInt, 
+            dbo.MOVCONT2.CntCod, 
+            dbo.CUENTAS.CntDsc, 
+            dbo.MOVCONT2.TrcCod, 
+            dbo.TERCEROS.TrcRazSoc, 
+            dbo.MOVCONT2.MvCVlr, 
+            dbo.MOVCONT2.MvCDocRf1, 
+            dbo.MOVCONT2.MvCDocRf2, 
+            dbo.MOVCONT2.MvCDet, 
+            dbo.MOVCONT2.MvCBse, 
+            dbo.MOVCONT2.MvCImpCod
+        FROM 
+            ((dbo.MOVCONT3 INNER JOIN dbo.MOVCONT2 ON 
+            (dbo.MOVCONT3.MCDpto = dbo.MOVCONT2.MCDpto) AND 
+            (dbo.MOVCONT3.MvCNro = dbo.MOVCONT2.MvCNro) AND 
+            (dbo.MOVCONT3.DOCCOD = dbo.MOVCONT2.DOCCOD) AND 
+            (dbo.MOVCONT3.EMPCOD = dbo.MOVCONT2.EMPCOD)) 
+            LEFT JOIN dbo.CUENTAS ON (dbo.MOVCONT2.CntCod = dbo.CUENTAS.CntCod) AND 
+            (dbo.MOVCONT2.CntVig = dbo.CUENTAS.CntVig)) 
+            LEFT JOIN dbo.TERCEROS ON dbo.MOVCONT2.TrcCod = dbo.TERCEROS.TrcCod
+        WHERE (((dbo.MOVCONT3.MvCFch) Between convert(datetime, '" . $input['fecha_inicio'] . "' ,101) And 
+        convert(datetime,'" . $input['fecha_final'] . "', 101)) AND ((dbo.MOVCONT3.MvCEst)<>'N') AND 
+        ((dbo.MOVCONT2.TrcCod)= '" . $num_id . "')) AND dbo.MOVCONT2.CntCod NOT LIKE '6%' ORDER BY dbo.MOVCONT3.MvCFch";
 
+        $info = DB::connection('sqlsrv_info')->select($query);
+    }
 }
